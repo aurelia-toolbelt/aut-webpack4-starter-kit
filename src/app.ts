d264cb80b1cbb3bf60af100239b0d33ad657e694
Saeed Ganji <shahabganji@gmail.com>
2018-06-06 11:20:53 +0430

    init

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+
+import "toastr/build/toastr.css";
+import "font-awesome/css/font-awesome.css";
+
+
+import { ToastrService } from 'aurelia-toolbelt';
+import { inject } from 'aurelia-dependency-injection';
+
+@inject(ToastrService)
+export class App {
+  message = 'Hello World!';
+
+  constructor(private ts: ToastrService) {
+
+  }
+
+  alertMessage() {
+    this.ts.success('Thanks buddy :+1:');
+  }
+}
